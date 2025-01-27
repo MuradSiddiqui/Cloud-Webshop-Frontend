a772466ae52865824844eb516e379c8742f33186
MuradSiddiqui <109691430+MuradSiddiqui@users.noreply.github.com>
2025-01-27 12:03:00 +0100

    final update

--- app/hooks.ts
+++ app/hooks.ts
@@ -1,21 +1,21 @@
 export const getProducts = async() =>{
-    const res = await fetch("http://127.0.0.1:8000/api/products");
+    const res = await fetch("https://a7f6x2hcc5.execute-api.eu-central-1.amazonaws.com/dev/api/products");
     const data = await res.json();
     return data
 }
 export const getProductById = async(id: string) =>{
-    const res = await fetch("http://127.0.0.1:8000/api/products/"+id);
+    const res = await fetch("https://a7f6x2hcc5.execute-api.eu-central-1.amazonaws.com/dev/api/products/"+id);
     const data = await res.json();
     return data
 }
 export const getProductsByParams = async(params: string) =>{
-    const res = await fetch("http://127.0.0.1:8000/api/products/?"+params);
+    const res = await fetch("https://a7f6x2hcc5.execute-api.eu-central-1.amazonaws.com/dev/api/products/?"+params);
     const data = await res.json();
     return data
 }
 
 export const getCategories = async() =>{
-    const res = await fetch("http://127.0.0.1:8000/api/categories");
+    const res = await fetch("https://a7f6x2hcc5.execute-api.eu-central-1.amazonaws.com/dev/api/categories");
     const data = await res.json();
     return data
 }
