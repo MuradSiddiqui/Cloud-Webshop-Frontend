f7aeaa7b5e1eb78c296c3113f69672047eaa9082
MuradSiddiqui <109691430+MuradSiddiqui@users.noreply.github.com>
2025-01-27 12:10:49 +0100

    New

--- app/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-export const metadata = {
-  title: "Sign In",
-};
-
-export const dynamicLayout = false;
-export default function Page() {
-  return <SignIn />;
-}
