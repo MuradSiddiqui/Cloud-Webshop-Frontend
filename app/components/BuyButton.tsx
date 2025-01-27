244826d26d3050a8338b395753b73aaea2adabdd
MuradSiddiqui <109691430+MuradSiddiqui@users.noreply.github.com>
2025-01-28 00:06:30 +0100

    FixButton

--- app/components/BuyButton.tsx
+++ app/components/BuyButton.tsx
@@ -4,7 +4,7 @@ import useCartStore from "../useCartStore";
 import { Button } from "@/components/ui/button";
 
 const BuyButton = ({ data }: { data: any }) => {
-  const stocks= data?.stocks.quantity
+  const stock= data?.stock.quantity
   const { cartItems, addToCart, removeFromCart, getCartItems } = useCartStore();
   const handleAddItem = async () => {
     const cart = await getCartItems();
@@ -14,7 +14,7 @@ const BuyButton = ({ data }: { data: any }) => {
 
   return (
     <div>
-      <Button disabled={stocks<=0?true:false} size={"sm"} onClick={handleAddItem}>
+      <Button disabled={stock<=0?true:false} size={"sm"} onClick={handleAddItem}>
         Buy
       </Button>
     </div>
